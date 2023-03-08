c368aad8c93e99ddef7af81b8e19f4edb7cd0b66
Nickolas Marquez <nickomarquez7@gmail.com>
2023-03-08 09:17:06 -0700

    Initialzed constant

--- lib/food_truck.rb
+++ lib/food_truck.rb
@@ -1,3 +1,6 @@
 class FoodTruck
   
+  def initialize(name)
+    @name = name
+  end
 end
---
+++ spec/food_truck_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require './lib/item'
+require './lib/food_truck'
+
+RSpec.describe FoodTruck do
+  describe '#Iteration 2' do
+    it 'exists and has attributes' do
+      food_truck = FoodTruck.new("Rocky Mountain Pies")
+
+      expect(food_truck).to be_an_instance_of(FoodTruck)
+
+
+
+    end
+  end
+
+
+
+
+
+
+
+end
+
+
